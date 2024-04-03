e6fb771cffd87a672030361aed834ab8ce4e30fd
Anderson <goandersonray@gmail.com>
2024-04-03 12:42:29 -0400

    switched to trading with S&P500 and made a fancy output with results vs just holding onto the stock

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ bot that simulates trading stocks using the Alpha Vantage Stock API
 put API Key generated at https://www.alphavantage.co/support/#
 in text file called "APIKEY.txt"
 run gendata.py and input the stock of choice to get the weekly stock prices from the last 20 years
-run main.py to run the stock bot which outputs the amount of money made
+run main.py to run the stock bot which outputs the amount of money made	
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ CLOSEKEY = "4. close"
 
 data_raw = json.load(open("data.json","r"))["Weekly Time Series"]
 data = []
-for k in data_raw:
+dates = list(data_raw.keys())[::-1]
+for k in dates:
 	data.append(float(data_raw[k][CLOSEKEY]))
 
 
-# best lower and upper bound is -3 and 2
+# best lower and upper bound is -3 and 2 for IBM
+# best lower and upper bound is -5.8 and 9.1 for S&P 500
 def calculatemoneymade(lowerbound,upperbound,starting_money=1):
 	money = starting_money
 	shares = 0
@@ -25,9 +27,15 @@ def calculatemoneymade(lowerbound,upperbound,starting_money=1):
 			avgslope+= (sample[i+1]-sample[i])/(samplesize-1)
 		
 		if avgslope < lowerbound:
+			#money += shares*data[week]
+			#shares = 0
+			#print(money+shares*data[week],"sell")
 			shares += money/data[week]
 			money = 0
 		if avgslope > upperbound:
+			#shares += money/data[week]
+			#money = 0
+			#print(money+shares*data[week],"buy")
 			money += shares*data[week]
 			shares = 0
 
@@ -35,10 +43,26 @@ def calculatemoneymade(lowerbound,upperbound,starting_money=1):
 
 	money += shares*data[-1]
 	inc_per_year = (((money/starting_money)**(1/len(data)))**52)*100 - 100
+	iflazymoney = starting_money*data[-1]/data[0]
+	iflazy_inc_per_year = (((iflazymoney/starting_money)**(1/len(data)))**52)*100 - 100
+	
 	
 	return {
+		"starting money":starting_money,
 		"money": money,
-		"inc per year":  inc_per_year
+		"inc per year":  inc_per_year,
+		"iflazymoney": iflazymoney,
+		"lazy inc per year": iflazy_inc_per_year
 	}
 
-print(calculatemoneymade(-3,2))
+
+result = calculatemoneymade(-5.8,9.1,1000)
+# testing out inc per year calculation
+#print((result["inc per year"]/100+1)**(int(dates[-1][:4])-int(dates[0][:4]))*result["starting money"],result["money"])
+print(f"from {dates[0]} to {dates[-1]}")
+print(f"bot inc per year: {result["inc per year"]:0.2f}%")
+print(f"from ${result["starting money"]:0.2f} to ${result["money"]:0.2f}")
+print(f"{result["money"]/result["starting money"]*100-100:0.0f}% increase")
+print(f"not selling inc per year: {result["lazy inc per year"]:0.2f}%")
+print(f"from ${result["starting money"]:0.2f} to ${result["iflazymoney"]:0.2f}")
+print(f"{result["iflazymoney"]/result["starting money"]*100-100:0.0f}% increase")
