d13430ab167309bbd94691fedb156e13c253a983
Anderson Ray <goandersonray@gmail.com>
2024-04-04 17:40:04 -0400

    added stock graph which shows when the bot buys and sells, and made the bot not try and buy or sell when it can't

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import matplotlib.pyplot as plt
 
 CLOSEKEY = "4. close"
 
@@ -8,6 +9,9 @@ dates = list(data_raw.keys())[::-1]
 for k in dates:
 	data.append(float(data_raw[k][CLOSEKEY]))
 
+plt.plot(dates,data)#dates,data)
+#maxprice = max(data)
+#minprice = min(data)
 
 # best lower and upper bound is -3 and 2 for IBM
 # best lower and upper bound is -5.8 and 9.1 for S&P 500
@@ -26,18 +30,15 @@ def calculatemoneymade(lowerbound,upperbound,starting_money=1):
 		for i in range(samplesize-1):
 			avgslope+= (sample[i+1]-sample[i])/(samplesize-1)
 		
-		if avgslope < lowerbound:
-			#money += shares*data[week]
-			#shares = 0
-			#print(money+shares*data[week],"sell")
+		if avgslope < lowerbound and money!=0:
 			shares += money/data[week]
 			money = 0
-		if avgslope > upperbound:
-			#shares += money/data[week]
-			#money = 0
-			#print(money+shares*data[week],"buy")
+			plt.text(week,data[week]+10,'B')
+		if avgslope > upperbound and shares!=0:
 			money += shares*data[week]
 			shares = 0
+			print("nword")
+			plt.text(week,data[week]+10,'S')
 
 		sample = sample[1:]+[data[week]]
 
@@ -57,12 +58,17 @@ def calculatemoneymade(lowerbound,upperbound,starting_money=1):
 
 
 result = calculatemoneymade(-5.8,9.1,1000)
+#print(result['inc per year'])
 # testing out inc per year calculation
-#print((result["inc per year"]/100+1)**(int(dates[-1][:4])-int(dates[0][:4]))*result["starting money"],result["money"])
+#print((result['inc per year']/100+1)**(int(dates[-1][:4])-int(dates[0][:4]))*result['starting money'],result['money'])
 print(f"from {dates[0]} to {dates[-1]}")
-print(f"bot inc per year: {result["inc per year"]:0.2f}%")
-print(f"from ${result["starting money"]:0.2f} to ${result["money"]:0.2f}")
-print(f"{result["money"]/result["starting money"]*100-100:0.0f}% increase")
-print(f"not selling inc per year: {result["lazy inc per year"]:0.2f}%")
-print(f"from ${result["starting money"]:0.2f} to ${result["iflazymoney"]:0.2f}")
-print(f"{result["iflazymoney"]/result["starting money"]*100-100:0.0f}% increase")
+print(f"bot inc per year: {result['inc per year']:0.2f}%")
+print(f"from ${result['starting money']:0.2f} to ${result['money']:0.2f}")
+print(f"{result['money']/result['starting money']*100-100:0.0f}% increase")
+print(f"not selling inc per year: {result['lazy inc per year']:0.2f}%")
+print(f"from ${result['starting money']:0.2f} to ${result['iflazymoney']:0.2f}")
+print(f"{result['iflazymoney']/result['starting money']*100-100:0.0f}% increase")
+
+
+#plt.xlabel('weeks')
+if input("display graph(y/n): ").lower() == "y": plt.show()
